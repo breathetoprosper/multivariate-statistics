7e086e7d0f731ae8e2501f0e9c54b87fda30e752
Diogo Marques <diogolmferreira@gmail.com>
2024-07-15 20:45:32 +0100

    Update TestModel.py
    refactored the code
    improved readability

--- TestModel.py
+++ TestModel.py
@@ -1,11 +1,12 @@
 # We use this file to classify new data based on the previously saved model
 # trained in StatsTests.py
-# The file was saved as random_forest_model.pkl.
+# The file was saved as random_forest_model2.pkl.
 # This file is in the same directory
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from StatsTests import COLUMNS_OF_INTEREST, df_val, df
 
 # Load the scaler, PCA, and trained model
 scaler = joblib.load('scaler.pkl')
@@ -15,8 +16,8 @@ model = joblib.load('random_forest_model.pkl')
 # Function to prepare new data and apply the model
 def predict_new_data(new_data):
     # Ensure new data has the same columns
-    columns_of_interest = ['Length', 'Left', 'Right', 'Bottom', 'Top', 'Diagonal']
-    new_data = new_data[columns_of_interest]
+    # columns_of_interest = ['Length', 'Left', 'Right', 'Bottom', 'Top', 'Diagonal']
+    new_data = new_data[COLUMNS_OF_INTEREST]
 
     # Scale and transform new data using the saved scaler and PCA
     X_scaled = scaler.transform(new_data)
@@ -27,15 +28,11 @@ def predict_new_data(new_data):
     
     return predictions
 
-# Example usage with new data
-new_data = pd.DataFrame({
-    'Length': [10.2, 15.4, 13.3],
-    'Left': [5.6, 7.1, 6.2],
-    'Right': [5.5, 7.0, 6.1],
-    'Bottom': [3.2, 4.0, 3.8],
-    'Top': [4.5, 5.3, 4.9],
-    'Diagonal': [11.0, 16.2, 14.5]
-})
+# predict with dv_tt
 
-predictions = predict_new_data(new_data)
+# predict with new data df_val
+predictions = predict_new_data(df_val)
 print(predictions)
+
+# print(len(df))
+# print(len(df_val))
